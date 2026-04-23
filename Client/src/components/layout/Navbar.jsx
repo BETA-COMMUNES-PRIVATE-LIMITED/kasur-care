9fb2f54a10fc3eee55285b5e085b8e34db84448a
rimsha_bibi <108316811+Rimshashehzadi@users.noreply.github.com>
2026-04-22 19:02:10 -0700

    add

--- Client/src/components/layout/Navbar.jsx
+++ Client/src/components/layout/Navbar.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { CgLayoutGridSmall } from "react-icons/cg";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { RxCross2 } from "react-icons/rx";
 import logo from '../../assets/logo.svg';
 import Wrapper from '../shared/Wrapper';
 const Navbar = () => {
+    const[isMenuOpen ,setIsMenuOpen] = useState(false)
+
+
+
     const Menu = [
-        { name: 'Home', link: '#' },
-        { name: 'Doctors', link: '#' },
-        { name: 'Patients', link: '#' },
-        { name: 'Pharmacy', link: '#' },
-        { name: 'Pages', link: '#' },
-        { name: 'Blog', link: '#' },
-        { name: 'Admin', link: '#' },
+        { name: 'Home' },
+        { name: 'Doctors' },
+        { name: 'Patients' },
+        { name: 'Pharmacy' },
+        { name: 'Pages' },
+        { name: 'Blog' },
+        { name: 'Admin' }
     ]
     return (
         <header className='w-full bg-white md:bg-transparent top-0 left-0 right-0'>
@@ -18,28 +24,89 @@ const Navbar = () => {
 
                 <div className='flex justify-between items-center py-4 px-6 md:px-12 lg:px-16 text-base gap-8 w-full'>
                     {/* logo */}
-                    <div>
+                    
                         <a href='' className='text-2xl font-semibold flex items-center space-x-3'>
                             <img src={logo} alt='' className='w-40 inline-block items-center' />
                         </a>
-                    </div>
+                    
                     {/* items */}
-                    <div className='group relative text-gray-500 text-lg'>
-                        <ul className='flex gap-x-7'>
-                            <li className='text-gray-600 hover:text-gray-800 cursor-pointer transition-colors duration-300'>Home</li>
-                            <li className='text-gray-600 hover:text-gray-800 cursor-pointer transition-colors duration-300'>About</li>
-                            <li className='text-gray-600 hover:text-gray-800 cursor-pointer transition-colors duration-300'>Services</li>
-                            <li className='text-gray-600 hover:text-gray-800 cursor-pointer transition-colors duration-300'>Contact</li>
 
-                        </ul>
-                    </div>
+                    <ul className=' gap-x-8 hidden xl:flex '>
+
+                        {Menu.map((items, index) => (
+                            <li key={index} className='  text-gray-700 flex text-lg font-medium  p-3 hover:animate-pulse cursor-pointer hover:bg-blue-400 hover:text-white rounded-md transition-all '>
+                                {items.name}
+
+                            </li>
+
+                        ))
+                        }
+                    </ul>
+
                     {/* button */}
                     <div>
-                        <CgLayoutGridSmall size={20} className='w-12 h-12 rounded-full  text-xs border border-gray-300 text-gray-500 hover:bg-blue-500 hover:text-white transition-colors duration-300 cursor-pointer flex items-center justify-center' />
+                        <CgLayoutGridSmall size={20} className='w-12 h-12 hidden xl:flex rounded-full hover:animate-pulse  text-xs border border-gray-300 text-gray-500 hover:bg-blue-400 hover:text-white transition-colors duration-300 cursor-pointer  items-center justify-center' />
 
                     </div>
+                    {/* for mobile menu */}
+                {/* <div className='xl:hidden block  cursor-pointer'>
+                   < RxHamburgerMenu onClick={()=> setIsMenuOpen(!isMenuOpen)} size={30}/> 
+                   <div className={`absolute xl:hidden top-24 left-0 w-full bg-white flex flex-col items-center gap-6 font-semibold text-lg transition-all duration-300 ${
+  isMenuOpen ? "opacity-100 visible" : "opacity-0 invisible"
+}`}
+                   style={{transition: "transform 0.5s ease,opacity 0.3s ease"}}>
+                     <ul className='flex flex-col items-center gap-6 w-full '>
+
+                        {Menu.map((items, index) => (
+                            <li key={index} className='  text-gray-700 flex text-lg font-medium  p-3 hover:animate-pulse cursor-pointer hover:bg-blue-400 hover:text-white rounded-md transition-all '>
+                                {items.name}
+
+                            </li>
+
+                        ))
+                        }
+                    </ul>
+
+                   </div>
+                </div> */}
+                <div className='xl:hidden block cursor-pointer'>
+ {isMenuOpen ? (
+  <RxCross2 
+    onClick={() => setIsMenuOpen(false)} 
+    size={30} 
+    className="transition-transform duration-300 rotate-180"
+  />
+) : (
+  <RxHamburgerMenu 
+    onClick={() => setIsMenuOpen(true)} 
+    size={30} 
+    className="transition-transform duration-300"
+  />
+)}
+  <div
+    className={`absolute xl:hidden top-24 left-0 w-full bg-white flex flex-col items-center gap-6 font-semibold text-lg transition-all duration-300 ${
+      isMenuOpen ? "translate-y-0 opacity-100" : "-translate-y-10 opacity-0"
+    }`}
+  >
+    
+
+    <ul className='flex flex-col items-center gap-6 w-full'>
+      {Menu.map((items, index) => (
+        <li
+          key={index}
+          onClick={() => setIsMenuOpen(false)}
+          className='text-gray-700 text-lg w-full list-none text-center p-4 font-medium  hover:bg-blue-400 hover:text-white rounded-md transition-all cursor-pointer'
+        >
+          {items.name}
+        </li>
+      ))}
+    </ul>
+  </div>
+</div>
                 </div>
-           </nav>
+                
+               
+            </nav>
         </header>
     )
 }
